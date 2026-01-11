5ed82081526bcea9804efd0766ea117daebcf92c
siraajul <shirajulislamparvez@gmail.com>
2026-01-11 12:43:32 +0600

    feat: fix sanity issues

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,5 +1,5 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 import { dataset, projectId } from '../env'
 
@@ -7,5 +7,5 @@ import { dataset, projectId } from '../env'
 const builder = createImageUrlBuilder({ projectId, dataset })
 
 export const urlFor = (source: SanityImageSource) => {
-    return builder.image(source)
+  return builder.image(source)
 }
