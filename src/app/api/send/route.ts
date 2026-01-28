9f5ae1043aca9c96f857a8faa004a9967116a025
siraajul <shirajulislamparvez@gmail.com>
2026-01-29 02:49:47 +0600

    fix: Update Resend API `from` email address to use `leads` subdomain.

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -55,7 +55,7 @@ export async function POST(request: Request) {
         }
 
         const data = await resend.emails.send({
-            from: 'Shift2Dynamic Leads <leads@shift2dynamic.com>',
+            from: 'Shift2Dynamic Leads <leads@leads.shift2dynamic.com>',
             to: ['business@shift2dynamic.com'],
             subject: `New Project Inquiry from ${name}`,
             react: await EmailTemplate({ formData: body }),
